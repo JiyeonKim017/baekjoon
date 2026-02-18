a5868e13dc1565a75e54e01c3609842108f8d4d4
JiyeonKim017 <jiyeon.fisa@gmail.com>
2026-02-18 16:50:30 +0900

    [level 2] Title: 프로세스, Time: 0.66 ms, Memory: 9.03 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,16 +1,16 @@
 from collections import deque
 
 def solution(priorities, location):
+    # (중요도, 인덱스) 튜플로 큐 생성
+    queue = deque( [p,i] for i, p in enumerate(priorities))
     answer = 0
-    queue = deque([ (p, i) for i, p in enumerate(priorities) ])
     
     while queue:
         current = queue.popleft()
         
-        if any(current[0] < item[0] for item in queue):
+        if any(current[0] < p[0] for p in queue):
             queue.append(current)
         else:
             answer += 1
             if current[1] == location:
                 return answer
-    return answer
